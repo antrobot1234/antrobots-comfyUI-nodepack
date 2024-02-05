b48935cbaa017ccf76036bbe959f4e56ebf0f818
antrobot1234 <antrobot1234@gmail.com>
2024-02-04 23:46:40 -0600

    updated custom KSampler to use booleans instead of enums

--- sampling_nodes.py
+++ sampling_nodes.py
@@ -16,8 +16,8 @@ class KSamplerWithDenoise:
     @classmethod
     def INPUT_TYPES(s):
                 return {"required":
-                    {"model": ("MODEL",),
-                    "add_noise": (["enable", "disable"], ),
+                    {"model": ("MODEL", ),
+                    "add_noise": ("BOOLEAN", {"default": True}),
                     "noise_seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
                     "steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
                     "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0, "step":0.1, "round": 0.01}),
@@ -28,7 +28,7 @@ class KSamplerWithDenoise:
                     "latent_image": ("LATENT", ),
                     "start_at_step": ("INT", {"default": 0, "min": 0, "max": 10000}),
                     "end_at_step": ("INT", {"default": 10000, "min": 0, "max": 10000}),
-                    "return_with_leftover_noise": (["disable", "enable"], ),
+                    "return_with_leftover_noise": ("BOOLEAN", {"default": False}),
                     "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step":0.01, "round": 0.01})
                      }
                 }
@@ -38,10 +38,10 @@ class KSamplerWithDenoise:
 
     def sample(self, model, add_noise, noise_seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, start_at_step, end_at_step, return_with_leftover_noise, denoise):
         force_full_denoise = True
-        if return_with_leftover_noise == "enable":
+        if return_with_leftover_noise:
             force_full_denoise = False
         disable_noise = False
-        if add_noise == "disable":
+        if add_noise:
             disable_noise = True
         return common_ksampler(model, noise_seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=denoise, disable_noise=disable_noise, start_step=start_at_step, last_step=end_at_step, force_full_denoise=force_full_denoise)
 
