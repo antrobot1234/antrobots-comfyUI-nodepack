bb36c885caaac1057f7c5a1e55766fdf9b21535a
antrobot1234 <antrobot1234@gmail.com>
2024-02-06 08:33:43 -0600

    re-factored dict nodes to function with a recipe system vs a bunch of manual class definitions.

--- dict_nodes.py
+++ dict_nodes.py
@@ -1,214 +1,101 @@
-from .globals import DIRECTORY_NAME
+from .globals import DIRECTORY_NAME, MAXSIZE, MINSIZE
 NODE_CLASS_MAPPINGS = {}
 GROUP_NAME = "dicts"
 from .any import Any
-ANY = Any("*")
-from .utils.image_utils import empty_image, empty_mask, is_mask, is_image
+any = Any("*")
+from .utils.image_utils import empty_image, empty_mask, is_mask, is_image, is_latent
 import torch
-class SetDict:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required":
+#SET DICT SECTION
+def get_first_value(values):
+    for value in values:
+        return value
+
+def set_return_helper(type_name,type_parameters={},type_label=None):
+    if type_label is None: type_label = type_name
+    return {"required":
                     {
                      "key":("STRING",{"multiline":False}),
-                     "value":(ANY,)
+                     type_label:(type_name,type_parameters)
                      },
                 "optional":
                     {
                      "DICT":("DICT",)
                     }
-                }
-    RETURN_TYPES = ("DICT",)
-    RETURN_NAMES = ("DICT",)
-    FUNCTION = "set"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def set(self, key, value, DICT = None):
-        if DICT is None: DICT = {}
-        DICT[key] = value
-        return (DICT,)
-NODE_CLASS_MAPPINGS["Set Dict"] = SetDict
-class GetDict:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required":
+                } 
+def get_return_helper(type_name,default_required,default_parameters=None):
+    out =  {"required":
                     {
                      "key":("STRING",{"multiline":False}),
-                     "DICT":("DICT",),
-                     "default":(ANY,)
-                     },
-                }
-    RETURN_TYPES = (ANY,)
-    RETURN_NAMES = ("*",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def get(self, key, DICT, default):
-        if key in DICT:
-            return (DICT[key],)
-        return (default,) 
-NODE_CLASS_MAPPINGS["Get Dict"] = GetDict
-class GetDictInt:
+                     "DICT":("DICT",)
+                    }}
+    if default_required:
+        out["required"].update(default_parameters)
+    else:
+        out["optional"].update(default_parameters)
+    return out
+def set_class_constructor(class_Name,pretty_name,type_value,type_parameters=None,type_label=None,type_class=None,type_checker=None):
     @classmethod
     def INPUT_TYPES(s):
-        return {
-            "required": {
-                "key": ("STRING", {"multiline": False}),
-                "DICT": ("DICT",)
-            },
-            "optional": {
-                "default": ("INT", {"default": 0})
-            }
-        }
-    RETURN_TYPES = ("INT",)
-    RETURN_NAMES = ("INT",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def get(self, key, DICT, default = 0):
-        if key in DICT and type(DICT[key]) == int:
-            return (DICT[key],)
-        return (default,)
-NODE_CLASS_MAPPINGS["Get Dict Int"] = GetDictInt
-class GetDictFloat:
+        return set_return_helper(type_value,type_parameters,type_label)
+    def set(self, key, DICT = {}, **kwargs):
+        value = get_first_value(kwargs.values())
+        if (type_class == None or type(value) == type_class) and (type_checker == None or type_checker(value)):
+            DICT[key] = value
+        return (DICT,)
+    attributes = {
+        "INPUT_TYPES":INPUT_TYPES,
+        "set":set,
+        "RETURN_TYPES":("DICT",),
+        "RETURN_NAMES":("DICT",),
+        "FUNCTION": "set",
+        "CATEGORY": DIRECTORY_NAME+'/'+GROUP_NAME+"/set"
+    }
+    class_out = type(class_Name,(object,),attributes)
+    NODE_CLASS_MAPPINGS[pretty_name] = class_out
+    return class_out
+def get_class_constructor(class_Name,pretty_name,type_value,default_parameters = None,type_label=None,type_class=None,type_checker=None,default_replacer=None, default_required = False):
+    if type_label is None: type_label = type_value
     @classmethod
     def INPUT_TYPES(s):
-        return {
-            "required": {
-                "key": ("STRING", {"multiline": False}),
-                "DICT": ("DICT",)
-            },"optional": {
-                "default": ("FLOAT", {"default": 0.0})
-            }
-        }
-    RETURN_TYPES = ("FLOAT",)
-    RETURN_NAMES = ("FLOAT",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def get(self, key, DICT, default = 0.0):
-        if key in DICT and type(DICT[key]) == float:
+        return get_return_helper(type_value,default_required,default_parameters)
+    def get(self, DICT,default=None, **kwargs):
+        key = get_first_value(kwargs.values())
+        if key is None: return (default,)
+        if key in DICT and (type_class == None or type(DICT[key]) == type_class) and (type_checker == None or type_checker(DICT[key])):
             return (DICT[key],)
-        return (default,)
-NODE_CLASS_MAPPINGS["Get Dict Float"] = GetDictFloat
-class GetDictBool:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "key": ("STRING", {"multiline": False}),
-                "DICT": ("DICT",)
-            },
-            "optional": {
-                "default": ("BOOL", {"default": False})
-            }
-        }
-    RETURN_TYPES = ("BOOL",)
-    RETURN_NAMES = ("BOOL",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def get(self, key, DICT, default = False):
-        if key in DICT and type(DICT[key]) == bool:
-            return (DICT[key],)
-        return (default,)
-NODE_CLASS_MAPPINGS["Get Dict Bool"] = GetDictBool
-class GetDictString:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "key": ("STRING", {"multiline": False}),
-                "DICT": ("DICT",)
-            },
-            "optional": {
-                "default": ("STRING", {"default": ""})
-            }
-        }
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("STRING",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def get(self, key, DICT, default = ""):
-        if key in DICT and type(DICT[key]) == str:
-            return (DICT[key],)
-        return (default,)
-NODE_CLASS_MAPPINGS["Get Dict String"] = GetDictString
-class GetDictImage:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "key": ("STRING", {"multiline": False}),
-                "DICT": ("DICT",)
-            },
-            "optional": {
-                "default": ("IMAGE",)
-            }
-        }
-    RETURN_TYPES = ("IMAGE",)
-    RETURN_NAMES = ("IMAGE",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def get(self, key, DICT, default = empty_image()):
-        if key in DICT and is_image(DICT[key]):
-            return (DICT[key],)
-        return (default,)
-NODE_CLASS_MAPPINGS["Get Dict Image"] = GetDictImage
-class GetDictMask:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "key": ("STRING", {"multiline": False}),
-                "DICT": ("DICT",)
-            },
-            "optional": {
-                "default": ("MASK",)
-            }
-        }
-    RETURN_TYPES = ("MASK",)
-    RETURN_NAMES = ("MASK",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def get(self, key, DICT,default = empty_mask()):
-        if key in DICT and is_mask(DICT[key]):
-            return (DICT[key],)
-        return (default,)
-NODE_CLASS_MAPPINGS["Get Dict Mask"] = GetDictMask
-class getDictDict:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "key": ("STRING", {"multiline": False}),
-                "DICT": ("DICT",)
-            },
-            "optional": {
-                "default": ("DICT",)
-            }
-        }
-    RETURN_TYPES = ("DICT",)
-    RETURN_NAMES = ("DICT",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME
-    def get(self, key, DICT, default = {}):
-        if key in DICT and type(DICT[key]) == dict:
-            return (DICT[key],)
-        return (default,)
-NODE_CLASS_MAPPINGS["Get Nested Dict"] = getDictDict
-class GetDictUnsafe:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "key": ("STRING", {"multiline": False}),
-                "DICT": ("DICT",)
-            },
-        }
-    RETURN_TYPES = (ANY,)
-    RETURN_NAMES = ("value",)
-    FUNCTION = "get"
-    CATEGORY = DIRECTORY_NAME+'/'+GROUP_NAME+'/EXPERIMENTAL'
-    def get(self, key, DICT):
-        if key in DICT:
-            return (DICT[key],)
-        else:
-            print("Warning: key [", key, "] not found in DICT. Unexpected behavior may occur.")
-            return (None,)
-NODE_CLASS_MAPPINGS["Get Dict Unsafe"] = GetDictUnsafe
+        if default is not None:
+            return (default,)
+        if default_replacer is not None:
+            return (default_replacer(),)
+        print('\033[93m'+"default not found. Unexpected behavior may occur.")
+        return (None,)
+    attributes = {
+        "INPUT_TYPES":INPUT_TYPES,
+        "get":get,
+        "RETURN_TYPES":(type_value,),
+        "RETURN_NAMES":(type_label,),
+        "FUNCTION": "get",
+        "CATEGORY": DIRECTORY_NAME+'/'+GROUP_NAME+"/get"
+    }
+    class_out = type(class_Name,(object,),attributes)
+    NODE_CLASS_MAPPINGS[pretty_name] = class_out
+    return class_out
+set_class_constructor("SetDict","Set Dict",any,type_label="any")
+set_class_constructor("setDictInt","Set Dict Int","INT",{"default":0},type_class=int)
+set_class_constructor("setDictFloat","Set Dict Float","FLOAT",{"default":0.0,"step":0.01},type_class=float)
+set_class_constructor("setDictBool","Set Dict Bool","BOOLEAN",{"default":False},type_class=bool)
+set_class_constructor("setDictString","Set Dict String","STRING",{"multiline":False},type_class=str)
+set_class_constructor("setDictImage","Set Dict Image","IMAGE",type_checker=is_image)
+set_class_constructor("setDictMask","Set Dict Mask","MASK",type_checker=is_mask)
+set_class_constructor("setDictLatent","Set Dict Latent","LATENT",type_checker=is_latent)
+#set_class_constructor("setDictModel","Set Dict Model","MODEL") #check if model?
+
+get_class_constructor("GetDict","Get Dict",any,type_label="any")
+get_class_constructor("getDictInt","Get Dict Int","INT",{"default":0},type_class=int)
+get_class_constructor("getDictFloat","Get Dict Float","FLOAT",{"default":0.0,"step":0.01},type_class=float)
+get_class_constructor("getDictBool","Get Dict Bool","BOOLEAN",{"default":False},type_class=bool)
+get_class_constructor("getDictString","Get Dict String","STRING",{"multiline":False},type_class=str)
+get_class_constructor("getDictImage","Get Dict Image","IMAGE",type_checker=is_image,default_replacer=empty_image)
+get_class_constructor("getDictMask","Get Dict Mask","MASK",type_checker=is_mask,default_replacer=empty_mask)
+get_class_constructor("getDictLatent","Get Dict Latent","LATENT",type_checker=is_latent)
+#get_class_constructor("getDictModel","Get Dict Model","MODEL")
