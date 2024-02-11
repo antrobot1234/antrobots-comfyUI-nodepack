43f2f2f18c884289eed15e1d47d22a62d7ccc62b
antrobot1234 <antrobot1234@gmail.com>
2024-02-11 15:47:43 -0600

    added an "unsafety" feature, where if a completely unparameterized entry is made, it will be classified as unsafe

--- utils/dict_utils.py
+++ utils/dict_utils.py
@@ -1,7 +1,8 @@
 from collections import UserDict
 class Entry:
     def __init__(self, value = None, typedef = None, default = None):
-        if typedef == None and value == None and default == None: raise Exception("Must specify at least one of value, typedef, or default")
+        if typedef == None and value == None and default == None: self.unsafe = True
+        else: self.unsafe = False
         if default != None and value == None: value = default
         if value != None:
             if typedef != None: 
@@ -16,9 +17,12 @@ class Entry:
         self.default = default
         self.value = value
     def __repr__(self) -> str:
-        return f"Entry(value={self.value}, typedef={self.typedef}, default={self.default})"
+        return f"{self.value}::{self.typedef.__name__}[{self.default}]"
     def is_type(self, type):
         return self.typedef == type
+    def type_equals(self, other):
+        if self.unsafe or other.unsafe: return True
+        return self.typedef == other.typedef
     def get_value(self):
         if self.value == None: return self.default
         return self.value
@@ -26,15 +30,14 @@ class EntryDict(UserDict):
     def __setitem__(self, key,item) -> None:
         if not isinstance(item, Entry):
             try:
-                if isinstance(item, dict):item = Entry(**item)
-                else:item = Entry(item)
+                item = Entry(item)
             except ValueError:
                 raise Exception(f"Could not convert {item} to Entry")
         return super().__setitem__(key, item)
     def get_by_reference(self, key, reference : Entry) -> Entry:
         entry = self.get(key, reference)
         if entry is not reference:
-            if entry.is_type(reference.typedef):
+            if entry.type_equals(reference):
                 return entry
             else: raise Exception(f"Entry {key} is not {reference.typedef.__name__}")
         return entry
