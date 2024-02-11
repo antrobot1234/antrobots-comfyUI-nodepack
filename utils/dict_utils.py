60abbed9de1c660f3f91fa7811d61b68f7f5528b
antrobot1234 <antrobot1234@gmail.com>
2024-02-11 05:19:19 -0600

    added an EntryDict class to better manage dictionary entries

--- utils/dict_utils.py
+++ utils/dict_utils.py
@@ -1,5 +1,6 @@
+from collections import UserDict
 class Entry:
-    def __init__(self, key, value = None, typedef = None, default = None):
+    def __init__(self, value = None, typedef = None, default = None):
         if value != None:
             if typedef != None: 
                 try: value = typedef(value)
@@ -9,7 +10,16 @@ class Entry:
             try: default = typedef() if default == None else typedef(default)
             except: raise Exception(f"Could not convert {default} to {typedef}")
         
-        self.key = key
         self.typedef = typedef
         self.default = default
         self.value = value
+    def __repr__(self) -> str:
+        return f"Entry(value={self.value}, typedef={self.typedef}, default={self.default})"
+class EntryDict(UserDict):
+    def __setitem__(self, key,item) -> None:
+        if not isinstance(item, Entry):
+            try:
+                if isinstance(item, dict):item = Entry(**item)
+                else:item = Entry(item)
+            except: raise Exception(f"Could not convert {item} to Entry")
+        return super().__setitem__(key, item)
