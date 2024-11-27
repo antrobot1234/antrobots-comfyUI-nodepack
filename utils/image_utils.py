9a4398ea30474a2a69940df95edebdf54abcd19b
antrobot1234 <antrobot1234@gmail.com>
2024-11-27 03:02:09 -0800

    convert mask to pil

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -366,6 +366,7 @@ def fill_with_color(image:torch.Tensor, mask:torch.Tensor, r:int, g:int, b:int) 
         torch.Tensor: The filled image tensor.
     """
     mask = scale_to_image(mask, image, TF.InterpolationMode.NEAREST_EXACT)
+    mask = convert_img_to_pil(mask)
     image = convert_img_to_pil(image)
     image_fill = Image.new('RGB', image.size, (r, g, b))
     image = Image.composite(image_fill, image, mask)
