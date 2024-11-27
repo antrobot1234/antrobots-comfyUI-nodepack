72fb213af35ad42f81eec669699064f4fcc4233d
antrobot1234 <antrobot1234@gmail.com>
2024-11-27 02:51:29 -0800

    scales the mask to the image size

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -365,6 +365,7 @@ def fill_with_color(image:torch.Tensor, mask:torch.Tensor, r:int, g:int, b:int) 
     Returns:
         torch.Tensor: The filled image tensor.
     """
+    mask = scale_to_image(mask, image, TF.InterpolationMode.NEAREST_EXACT)
     image = convert_img_to_pil(image)
     mask = convert_img_to_pil(mask)
     image_fill = Image.new('RGB', image.size, (r, g, b))
